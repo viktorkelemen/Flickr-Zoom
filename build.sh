21db96da9474f7e296140c1963fbbb3f87ce1563
Viktor Kelemen <viktor@cirius.co.jp>
2010-08-09 16:31:13 +0900

    restructuring the directories

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Makes it ready to be uploaded
 
-cd ./src
+cd ./src/chrome-extension
 
 # version number of the extension
 version=`eval cat manifest.json | grep "version" | cut -d \" -f 4`
 
 # compressing
 zip flickr-zoom-chrome_$version.zip *
-mv flickr-zoom-chrome_$version.zip ../bin
-cd ..
+mv flickr-zoom-chrome_$version.zip ../../bin
+cd ../..
